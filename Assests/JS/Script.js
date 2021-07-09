e4e595c9e04fd57bb852c02a92c2f16b1b2ca0cd
Elziekio <anthony.deamer@hotmail.com>
2021-07-10 09:14:31 +1000

    Updating files and routine commit

---
+++ Assests/JS/Script.js
@@ -0,0 +1,114 @@
+//bringing over HTML elements from the game.html//
+
+var question = document.querySelector("#question");
+var choices = document.querySelectorAll(".choice-text");
+var choicebtns = document.querySelectorAll(".choice-container");
+var highscores = document.querySelector("#highscores");
+
+
+
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var questionCounter = 0;
+var availableQuestions = {};
+
+//inputting questions inside the the question div in game.html//
+
+ let questions = [
+    {
+        question: "What can a Boolean values be?",
+        choice1: "True or False",
+        choice2: "Yes or No",
+        choice3: "String only",
+        choice4: "Numbers",
+        answer: 1,
+    },
+    {
+        question: "What is an if statment?",
+        choice1: "Function",
+        choice2: "Variable",
+        choice3: "Loop",
+        choice4: "Conditional Statement",
+        answer: 4,
+    },
+    {
+        question: "What does != mean?",
+        choice1: "Equals to",
+        choice2: "Not equal to",
+        choice3: "True",
+        choice4: "Multiply",
+        answer: 2,
+    },
+    {
+        question: "What does the Console.log do?",
+        choice1: "Enables a loop funtion to happen",
+        choice2: "A way to call a function",
+        choice3: "Shows a value of a nominated variable in the console tab in devtools",
+        choice4: "Use to pull a HTML element into the JS file to be manipulated",
+        answer: 3,
+    }
+];
+
+
+
+var MAX_QUESTIONS = 4;
+
+function startgame () {
+    questionCounter = 0
+    availableQuestions = [...questions]
+    getNewQuestions(0)
+};
+
+function getNewQuestions(questionsIndex) {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+
+        return window.location.assign("./end.html")
+    }
+
+    currentQuestions = availableQuestions[questionsIndex]
+    question.textContent = currentQuestions.question
+    
+    choices.forEach(choice => {
+        const number = choice.dataset["number"]
+        choice.innerText = currentQuestions["choice" + number]
+    })
+    availableQuestions.splice(questionsIndex, 1)
+    console.log(availableQuestions)
+
+    acceptingAnswers = true
+};
+
+choicebtns.forEach(item => {
+    console.log('item', item)
+    item.addEventListener("click", function() {
+        getNewQuestions(0)
+        console.log("clicked")
+    })
+})
+
+//function for the timer//
+var timeleft = 30;
+var downloadTimer = setInterval(function(){
+    console.log(timeleft);
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    if (window.confirm('Game Over!'))
+    {
+        alert('Set Highscore');
+        window.location = './end.html';
+    }
+    else
+    {
+        die();
+    }
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft;
+  }
+  timeleft -= 1;
+}, 1000);
+
+
+
+//calling the start game function//
+startgame();
---
+++ Assests/JS/end.js
@@ -0,0 +1,31 @@
+var initials = document.querySelector("#initials");
+var submitbtn = document.querySelector("#submitbtn");
+var highscores = document.querySelector("#highscores");
+var HSbtn = document.querySelector("#HSbtn");
+var timer = document.querySelector("#countdown");
+
+
+window.localStorage.setItem("Initial", initials);
+window.localStorage.setItem("Time Stamp", timer);
+
+// When either the submit or highscore button is pressed on the end.html the input screen will change to the highscore screen.
+submitbtn.onclick = function(){
+    console.log(submitbtn)
+    highscores.classList.remove("hidden");
+    document.getElementById("Results").classList.add("hidden");
+};
+
+HSbtn.onclick = function(){
+    console.log(HSbtn)
+    highscores.classList.remove("hidden");
+    document.getElementById("Results").classList.add("hidden");
+};  
+
+(function manage(initials) {
+   if (initials.value != " "){
+        submitbtn.disabled = false;
+   }
+   else {
+        submitbtn.disabled = true;
+   }
+})
